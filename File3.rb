057de71093b20b112be7951d9eba8fee3ce1ff98
huisoido <584331125@qq.com>
2017-03-22 15:34:20 +0800

    some ruby demos

---
+++ File3.rb
@@ -0,0 +1,5 @@
+arr = IO.readlines("input.txt")
+puts arr[0]
+puts arr[1]
+
+IO.foreach("input.txt"){|block| puts block}
---
+++ Time.rb
@@ -0,0 +1,5 @@
+time1 = Time.new
+puts "当前时间 : " + time1.inspect
+
+time2 = Time.now
+puts "当前时间 ：" + time2.inspect
